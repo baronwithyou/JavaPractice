04792c28d7af534b28ab4fe3142d6aaaf55229a2
githubbaron <linsi.yao@163.com>
2017-11-20 18:30:27 +0800

    添加12章练习

---
+++ src/Chapter11/TestExceptions.java
@@ -0,0 +1,23 @@
+package Chapter11;
+
+public class TestExceptions {
+    public static void main(String []args) {
+        try {
+            doRisky("yes");
+            // 如果上一条语句抛出异常 则不会运行下一行语句
+            System.out.println("end try");
+        } catch (Exception e) {
+            System.out.println("scary exception");
+        } finally {
+            System.out.println("finally");
+        }
+    }
+
+    static void doRisky(String test) throws Exception {
+//        System.out.println("Start risky");
+        if ("yes".equals(test)) {
+            throw new Exception();
+        }
+//        System.out.println("end risky");
+    }
+}
